021bc3ee6abc9727d8c2f82502220dd220c7ec79
eden45676 <edenliu94@gmail.com>
2020-05-05 12:18:30 -0700

    firstTime

---
+++ js/JavaScript.js
@@ -0,0 +1,40 @@
+function myFunction() {
+    window.alert('This is a cat');
+  }
+
+function hamster(){
+    
+    document.getElementById("hamster").src="asset/hamster2.gif";
+    
+    
+}
+//drag and drop
+function allowDrop(ev) {
+    ev.preventDefault();
+  }
+  
+  function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+  
+  function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+    document.getElementById("hamster").src="asset/hamster2.gif";
+  }
+
+  function toggleShow() {
+    var x = document.getElementById("uvicDIV");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+    var y = document.getElementById("degreeDIV");
+    if (y.style.display === "none") {
+      y.style.display = "block";
+    } else {
+      y.style.display = "none";
+    }
+  }
